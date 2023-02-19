c03619a5c01d6d89c7af5b2f100369574de7f54f
Aliyu AbdulGaniy <alialaba079@gmail.com>
2023-02-19 02:09:14 -0800

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* Betty doc style */
+/**
+ * main - print base 10
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0 ; num <= 9 ; num++)
+		printf("%d", num);
+	printf("\n");
+	return (0);
+}
