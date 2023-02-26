d1c7037b3d828143983b89570aad0d7e8902b200
Aliyu AbdulGaniy <alialaba079@gmail.com>
2023-02-26 09:20:25 -0800

    103-fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - finds and prints the sum of the even-valued terms followed by new line
+ * main - finds and prints the sum of the even-valued terms
  *
  * followed by a new line
  *
@@ -12,11 +12,9 @@
 		int i;
 		unsigned long int j, k, next, sum;
 
-
 		j = 1;
 		k = 2;
 		sum = 0;
-
 
 		for (i = 1; i <= 33; ++i)
 		{
@@ -28,10 +26,7 @@
 			j = k;
 			k = next;
 		}
-
-
 		printf("%lu\n", sum);
-
 
 		return (0);
 	}
