29c3de80193a483f7dc941d41fd4feee0b48c863
Aliyu AbdulGaniy <alialaba079@gmail.com>
2023-02-26 05:39:42 -0800

    102-fibonacci.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * main  print_times_table - prints the n times table, starting with 0
- *
- * @n: number of the times table
+ * main -  print_times_table - prints the n times table, starting with 0
+ * 
+ * Resturn always success
  */
 	void print_times_table(int n)
 	{
